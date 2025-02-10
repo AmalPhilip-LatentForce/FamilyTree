0c6bf8005d56f1b98d33f5cde6ed369e510f5655
AmalPhilip-LatentForce <amal.philip@latentforce.ai>
2025-02-10 14:04:24 +0530

    FamilyTree

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FamilyTree from './components/FamilyTree';
+
+
+function App() {
+  return (
+    <div className="App">
+      <FamilyTree/> 
+    
+    </div>
+  );
+}
+
+export default App;
